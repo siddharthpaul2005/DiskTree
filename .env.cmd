062db8eb70407edee4ce34a171555c8f692eb847
siddharthpaul2005 <paulsiddharth000@gmail.com>
2026-05-30 23:41:28 +0530

    chore: clean repo, fix gitignore, remove build artifacts

--- .env.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-set "PATH=C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja;%PATH%"
-set "VCPKG_ROOT=D:\vcpkg"
--- build/dev/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/disktree/tests
-# Build directory: D:/disktree/build/dev/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/disktree/build/dev/tests/disktree_tests-b12d07c_include.cmake")
